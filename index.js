5c913a88dbc37885edcc2d88209468c0d57f629d
Yuxiang Li <seanliyuxiang@gmail.com>
2021-08-30 16:55:16 -0500

    commit tree e735d5ae09f545c2dc67380e277288e961426e1c

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31, num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() + 2);
+
+let num3 = 9, num4 = 5;
+let mod = 9 % 5;
+
+let max = Math.max(-4, -77, 20, 4, 13, 14);
